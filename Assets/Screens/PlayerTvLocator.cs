e4a50e45c48033bac738db3a17e90064c0d9c119
Alex <alksgud@gmail.com>
2024-04-19 19:27:08 +0700

    Add screen locator

---
+++ Assets/Screens/PlayerTvLocator.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class PlayerTvLocator :MonoBehaviour
+{
+    public TV[] screens;
+    public bool deleteTool = false;
+
+    private TV _selected;
+    private void Awake()
+    {
+        _selected = screens[0];
+    }
+    public void SetScreen(int indx) 
+    {
+        switch (indx)
+        {
+            case 22:
+                _selected = screens[0];
+                deleteTool = false;
+                return;
+
+            case 27:
+                _selected = screens[1];
+                deleteTool = false;
+                return;
+
+            case 55:
+                _selected = screens[2];
+                deleteTool = false;
+                return;
+
+            case 0:
+                _selected = screens[0];
+                deleteTool = true;
+
+                return;
+        }
+    }
+
+    public void WallClick(Wall wall) 
+    {
+        if (deleteTool)
+            return;
+
+        var newTv = Instantiate(_selected);
+
+        newTv.transform.position = wall.transform.position;
+        newTv.SetWall(wall.televisionRotationAngle, wall.xorZWallFixedCoor,this);
+    }
+}
+
---
+++ Assets/Screens/TV.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.MixedReality.Toolkit.UI;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using System.Data;
+using UnityEngine;
+
+public class TV : MonoBehaviour
+{
+    [SerializeField] private Collider boxCol;
+    [SerializeField] private MoveAxisConstraint moveAxisConstraint;
+    [SerializeField] private Interactable interactable;
+
+    private bool located;
+    private PlayerTvLocator player;
+
+    public bool Located
+    {
+        get { return located; }
+        set
+        {
+            located = value;
+
+            if (value)
+                boxCol.enabled = true;
+            else
+                boxCol.enabled = false;
+        }
+    }
+
+    public void SetWall(float rot,XorZWallFixedCoor XorZ, PlayerTvLocator player) 
+    {
+        this.player = player;
+        interactable.OnClick.AddListener(Delete);
+
+
+        transform.rotation = Quaternion.Euler(0f, rot, 0f);
+
+        if (XorZ == XorZWallFixedCoor.Z) 
+        {
+            moveAxisConstraint.ConstraintOnMovement = AxisFlags.ZAxis;
+        }
+        else 
+        {
+            moveAxisConstraint.ConstraintOnMovement = AxisFlags.XAxis;
+        }
+    }
+    
+    public void Delete() 
+    {
+        if (player.deleteTool) 
+        {
+            Destroy(this.gameObject);
+            interactable.OnClick.RemoveListener(Delete);
+        }
+        
+    }
+}
+
